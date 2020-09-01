efc9be6d608992339fd058623a59c4a5ade1d012
demkom58 <demkom58@gmail.com>
2020-09-01 10:51:40 +0300

    Init laboratory work 8

---
+++ Lab8/src/main/java/com/demkom58/lab8/model/IWeight.java
@@ -0,0 +1,7 @@
+package com.demkom58.lab8.model;
+
+import java.io.Serializable;
+
+public interface IWeight extends Serializable {
+    float weight();
+}
---
+++ Lab8/src/main/java/com/demkom58/lab8/store/ProductStore.java
@@ -0,0 +1,49 @@
+package com.demkom58.lab8.store;
+
+import com.demkom58.lab8.event.IProductListener;
+import com.demkom58.lab8.event.ProductEvent;
+import com.demkom58.lab8.model.IWeight;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ProductStore implements Serializable {
+    private final Object monitor = new Object();
+
+    private List<IWeight> arr = new ArrayList<>();
+    private List<IProductListener> productListeners = new CopyOnWriteArrayList<>();
+
+    public void add(IWeight abstractForm) {
+        synchronized (monitor) {
+            arr.add(abstractForm);
+            fireProductListener(new ProductEvent(this , abstractForm));
+        }
+    }
+
+    public IWeight[] getArr() {
+        return arr.toArray(new IWeight[0]);
+    }
+
+    public void addProductListener(IProductListener listener) {
+        productListeners.add(listener);
+    }
+
+    public void removeProductListener(IProductListener listener) {
+        productListeners.remove(listener);
+    }
+
+    protected void fireProductListener(ProductEvent event) {
+        productListeners.forEach(listener -> listener.onProductEvent(event));
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder("Каталог виробів: \n");
+        for (IWeight iWeight : arr)
+            builder.append(iWeight).append("\n");
+
+        return builder.toString();
+    }
+}
